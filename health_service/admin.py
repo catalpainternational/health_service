c6b15d03be0a70d2fa5c1a7912f221c4dfa93bac
Anders Hofstee <a.hofstee@catalpainternational.org>
2012-10-15 14:19:47 +0900

    removed unused item from import statement

--- health_service/admin.py
+++ health_service/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from health_service.models import HealthFacilityType, HealthFacility
 from health_service.models import OwnershipType
-from health_service.lib.autocomplete_admin import FkAutocompleteAdmin, InlineAutocompleteAdmin
+from health_service.lib.autocomplete_admin import FkAutocompleteAdmin
 
 
 class OwnershipTypeAdmin(admin.ModelAdmin):
@@ -20,7 +20,7 @@ class HealthFacilityAdmin(FkAutocompleteAdmin):
     search_fields = ['name',]
     related_search_fields = {'area': ('^name',),
                              'parent': ('^name',),}
-    
+
 
 admin.site.register(OwnershipType, OwnershipTypeAdmin)
 admin.site.register(HealthFacility, HealthFacilityAdmin)
